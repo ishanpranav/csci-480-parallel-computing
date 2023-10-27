2126ea992f91f117215e393d88719f6820aa8006
Ishan Pranav <ishan.pranav@outlook.com>
2023-10-27 01:00:16 -0400

    Make func.h documentation more precise

--- src/func.h
+++ src/func.h
@@ -1,5 +1,5 @@
 /**
- * Represents a function whose input is one double and whose output is one
- * double.
+ * Represents a function whose input is one double-precision floating-point
+ * number and whose output is a double-precision floating-point number.
  */
 typedef double (*DoubleDoubleFunc)(double value);
