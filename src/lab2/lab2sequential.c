92aae6304a62d49fb66f20ce317190b29a216cf3
Ishan Pranav <ishan.pranav@outlook.com>
2023-11-15 22:08:36 -0500

    Add lab2

---
+++ src/lab2/lab2sequential.c
@@ -0,0 +1,14 @@
+#include "../core.h"
+
+/**
+ * The main entry point for the application.
+ *
+ * @param count the number of command-line arguments.
+ * @param args  a collection of command-line arguments. The length of the
+ *              collection is given by the `count` parameter.
+ * @return An exit code. This value is always 0, indicating success.
+ */
+int main(int count, String args[])
+{
+    
+}
