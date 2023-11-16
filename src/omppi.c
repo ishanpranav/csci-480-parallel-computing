53959c58d50ebedba44383ed27ea26fbb700fe67
Ishan Pranav <ishan.pranav@outlook.com>
2023-11-15 20:52:16 -0500

    Add random

---
+++ src/omppi.c
@@ -0,0 +1,35 @@
+#include <omp.h>
+#include <stdio.h>
+#include "core.h"
+#include "int_array.h"
+
+/**
+ * The main entry point for the application.
+ *
+ * @param count the number of command-line arguments.
+ * @param args  a collection of command-line arguments. The length of the
+ *              collection is given by the `count` parameter.
+ * @return This value is 0, indicating success, or 1, indicating
+ *         a usage error.
+ */
+int main(int count, String args[])
+{
+    if (count != 3)
+    {
+        fprintf(stderr, "usage: omppi <iterations> <block size>\n");
+
+        return 1;
+    }
+
+    int n = atoi(args[1]);
+    int blockSize = atoi(args[2]);
+    double t0 = omp_get_wtime();
+    IntArray durations = int_array(n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        durations[i] = 1 + (rand() % 5);
+    }
+
+    return 0;
+}
---
+++ src/random.h
@@ -0,0 +1,27 @@
+/**
+ * Represents a pseudo-random number generator, which is an algorithm that
+ * produces a sequence of numbers that meet certain statistical requirements
+ * for randomness.
+ */
+typedef unsigned int *Random;
+
+/**
+ * Initializes a new instance of the `Random` class using a default seed value.
+ * 
+ * @return A new instance of the `Random` class.
+*/
+Random random();
+
+/**
+ * Returns a random integer that is within a specified range.
+ * 
+ * @param instance the `Random` instance.
+ * @param minValue the inclusive lower bound of the random number returned.
+ * @param maxValue the exclusive upper bound of the random number returned.
+ *                 `maxValue` must be greater than or equal to `minValue`.
+ * @return A 32-bit signed integer greater than or equal to `minValue` and less
+ *         than `maxValue`; that is, the range of return values includes
+ *         `minValue` but not maxValue. If `minValue` equals `maxValue`,
+ *         `minValue` is returned.
+*/
+int random_next(Random instance, int minValue, int maxValue);
